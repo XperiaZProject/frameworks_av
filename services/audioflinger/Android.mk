7dd1a7c0ac798b8e7eede222a7781d81f16916e4
Dan Albert <danalbert@google.com>
2014-11-20 11:31:17 -0800

    C++11 compatibility.
     * Fix string literal concatenation to not be interpreted as UD
       literals.
     * Add constexpr compatibility for non-integral static members.
     * Use __typeof__ instead of typeof (should become decltype once this
       actually becomes C++11).
     * Add an appropriate cast for atomic_uintptr_t, since moving to C++11
       means moving from <stdatomic.h> to <atomic>, which has better
       typechecking (hooray for not macros!).
    
    Bug: 18466763
    Change-Id: I9561dcb2526578687819ff85421ba80d8e1a9694
    
    Also fix some compilation errors by enforcing C++11.
    
    Signed-off-by: arter97 <qkrwngud825@gmail.com>
    Signed-off-by: Chet Kener <Cl3Kener@gmail.com>
    
    Conflicts:
    	cmds/stagefright/SineSource.h
    	services/audioflinger/Android.mk
    	services/audioflinger/AudioMixer.h
    
    Signed-off-by: Chet Kener <Cl3Kener@gmail.com>
    
    Conflicts:
    	cmds/stagefright/SineSource.h
    	include/media/ToneGenerator.h

--- services/audioflinger/Android.mk
+++ services/audioflinger/Android.mk
@@ -65,8 +65,15 @@ LOCAL_STATIC_LIBRARIES := \
     libcpustats \
     libmedia_helper
 
-LOCAL_CFLAGS += -Os
-    
+#QTI Resampler
+ifeq ($(call is-vendor-board-platform,QCOM),true)
+ifeq ($(strip $(AUDIO_FEATURE_ENABLED_EXTN_RESAMPLER)),true)
+LOCAL_CFLAGS += -DQTI_RESAMPLER
+endif
+endif
+#QTI Resampler
+
+LOCAL_CFLAGS += -std=gnu++11
 LOCAL_MODULE:= libaudioflinger
 LOCAL_32_BIT_ONLY := true
 
@@ -132,6 +139,7 @@ LOCAL_SHARED_LIBRARIES := \
     liblog
 
 LOCAL_MODULE := libaudioresampler
+LOCAL_CFLAGS += -std=gnu++11
 
 include $(BUILD_SHARED_LIBRARY)
 
